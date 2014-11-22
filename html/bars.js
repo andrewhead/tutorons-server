39d7bf94c859f7f166236e798d73be91864359b0
Andrew Head <head.andrewm@gmail.com>
2014-11-21 21:02:05 -0800

    UI: Added axis and scaling to aggregate bars

--- html/bars.js
+++ html/bars.js
@@ -10,28 +10,24 @@ $(function () {
     };
 
  	// width and height
- 	var width = 500;
- 	var height = '100%'; // would depend on number of responses
-
 	var svg = d3.select("#search_bars")
-				.append("svg")
-				.attr("width", width)
-				.attr("height", height);
+        .append("svg")
+        .attr("width", "100%")
+        .attr("height", "100%");
 
 	d3.json(DATA_FILE_PATH, function(error, data) {
-		console.log(error);
 		var responses = svg.selectAll("g")
-						.data(data)
-						.enter()
-						.append("g")
+            .data(data)
+            .enter()
+            .append("g")
 
 		var bars = responses.selectAll("rect")
-						.data(function(d) { return d.lines; })
-						.enter()
-						.append("rect");
+            .data(function(d) { return d.lines; })
+            .enter()
+            .append("rect");
 
-	    var bar_width = 20;
-	    var bar_height = 50;
+	    var bar_width = 15;
+	    var bar_height = 20;
 	    var bar_horizontal_padding = 1;
 	    var bar_vertical_padding = 5;
 		var bar_attributes = bars
@@ -41,43 +37,72 @@ $(function () {
 			.attr("height", bar_height)
 			.style("fill", function(d) { return linetype_2_color[d.type_]; });
 
-        var refCounts = {};
-        for (var i = 0; i < data.length; i++) {
-            var lines = data[i].lines;
-            for (var j = 0; j < lines.length ; j++) {
-                var references = lines[j].references;
-                for (var k = 0; k < references.length; k++) {
-                    var ref = references[k];
-                    if (!(ref in refCounts)) {
-                        refCounts[ref] = 0;
+        function extract_reference_counts(data) {
+            var ref_counts = {};
+            for (var i = 0; i < data.length; i++) {
+                var lines = data[i].lines;
+                for (var j = 0; j < lines.length ; j++) {
+                    var references = lines[j].references;
+                    for (var k = 0; k < references.length; k++) {
+                        var ref = references[k];
+                        if (!(ref in ref_counts)) {
+                            ref_counts[ref] = 0;
+                        }
+                        ref_counts[ref]++;
                     }
-                    refCounts[ref]++;
                 }
             }
+            return ref_counts;
         }
 
-        var acw = 800;
-        var ach = 400;
-        var ac_bar_width = 20;
-        var ac_bar_horiz_padding = 5;
+        function px_to_num(str) {
+            return Number(str.replace("px", ""));
+        }
 
-        var acSvg = d3.select("#aggregate_chart")
-                    .append("svg")
-                    .attr("width", acw)
-                    .attr("height", ach);
-
-        var sortedRefCounts = d3.entries(refCounts).sort(function(a, b) {
+        var ref_counts = extract_reference_counts(data);
+        var sorted_ref_counts = d3.entries(ref_counts).sort(function(a, b) {
             return b.value - a.value;
         })
         
+        var aggregate_chart = d3.select("#aggregate_chart");
+        var acw = px_to_num(aggregate_chart.style("width"));
+        var ach = px_to_num(aggregate_chart.style("height"));
+        var ac_bar_width = 20;
+        var ac_bar_horiz_padding = 5;
+        var ac_edge_padding = 20;
+
+        var acSvg = aggregate_chart
+            .append("svg")
+            .attr("width", acw)
+            .attr("height", ach);
+
+        var max_count = d3.max(d3.entries(ref_counts), function(d) { return d.value; })
+        var yScale = d3.scale.linear()
+            .domain([0, max_count])
+            .range([ach - ac_edge_padding, ac_edge_padding]);
+        var hScale = d3.scale.linear()
+            .domain([0, max_count])
+            .range([0, ach - ac_edge_padding * 2]);
+
 		var acBars = acSvg.selectAll("rect")
-                    .data(sortedRefCounts)
-                    .enter()
-                    .append("rect")
-                    .attr("x", function (d, i) { return i * (ac_bar_width + ac_bar_horiz_padding); })
-                    .attr("y", function (d) { return ach - (10 * d.value); })
-                    .attr("width", ac_bar_width)
-                    .attr("height", function(d) { return 10 * d.value; })
-                    .style("fill", function(d) { return "blue"; });
+            .data(sorted_ref_counts)
+            .enter()
+            .append("rect")
+            .attr("x", function (d, i) { 
+                return ac_edge_padding + i * (ac_bar_width + ac_bar_horiz_padding); 
+            })
+            .attr("y", function (d) { return yScale(d.value); })
+            .attr("width", ac_bar_width)
+            .attr("height", function(d) { return hScale(d.value); })
+            .style("fill", function(d) { return "blue"; });
+        
+        var yAxis = d3.svg.axis()
+            .scale(yScale)
+            .ticks(4)
+            .orient("left");
+        acSvg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + ac_edge_padding + ",0)")
+            .call(yAxis);
 	});
 });
