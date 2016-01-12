ff6b5a5fac54ffe00102ccb437665e25be773ca0
mwaliman <mwaliman@berkeley.edu>
2016-01-11 16:52:49 -0800

    Python: added test_query, more cleanup

---
+++ tutorons/tests/python/test_detect.py
@@ -0,0 +1,96 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+from __future__ import unicode_literals
+import logging
+import unittest
+import json
+from django.test import Client
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+class PythonBuiltInDetectionTest(unittest.TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def get_regions(self, document):
+        resp = self.client.post(
+            '/python/scan',
+            data={'origin': 'www.test.com', 'document': document})
+        regions = json.loads(resp.content)
+        return regions
+
+    def test_single_built_in(self):
+        string = "<html> <body> <code>abs(2)</code> </body> </html>"
+        regions = self.get_regions(string)
+
+        self.assertEqual(len(regions), 1)
+        r = regions[0]
+        self.assertEqual(
+            r['node'],
+            'HTML > BODY:nth-of-type(1) > CODE:nth-of-type(1)')
+        self.assertEqual(r['start_index'], 0)
+        self.assertEqual(r['end_index'], 2)
+
+    def test_multiple_built_ins(self):
+        string = "<html> <body> <code>abs(2)\nlen('fdsjkfds')\nbin(1)</code> </body> </html>"
+        regions = self.get_regions(string)
+
+        self.assertEqual(len(regions), 3)
+        r0 = regions[0]
+        self.assertEqual(
+            r0['node'],
+            'HTML > BODY:nth-of-type(1) > CODE:nth-of-type(1)')
+        self.assertEqual(r0['start_index'], 0)
+        self.assertEqual(r0['end_index'], 2)
+
+        r1 = regions[1]
+        self.assertEqual(
+            r1['node'],
+            'HTML > BODY:nth-of-type(1) > CODE:nth-of-type(1)')
+        self.assertEqual(r1['start_index'], 7)
+        self.assertEqual(r1['end_index'], 9)
+
+        r2 = regions[2]
+        self.assertEqual(
+            r2['node'],
+            'HTML > BODY:nth-of-type(1) > CODE:nth-of-type(1)')
+        self.assertEqual(r2['start_index'], 23)
+        self.assertEqual(r2['end_index'], 25)
+
+    def test_binary_op_built_ins(self):
+        string = "<html> <body> <code>abs(2) + len('fdsjkfds') </code> </body> </html>"
+        regions = self.get_regions(string)
+
+        self.assertEqual(len(regions), 2)
+        r0 = regions[0]
+        self.assertEqual(r0['start_index'], 0)
+        self.assertEqual(r0['end_index'], 2)
+
+        r1 = regions[1]
+        self.assertEqual(r1['start_index'], 9)
+        self.assertEqual(r1['end_index'], 11)
+
+    def test_ignore_malformed_calls(self):
+        string = "<html> <body> <code>abs(2</code> </body> </html>"
+        regions = self.get_regions(string)
+        self.assertEqual(len(regions), 0)
+
+    def test_detect_builtin_after_increment(self):
+        string = "<html> <body> <code>x = 2\nx += abs(-1)</code> </body> </html>"
+        regions = self.get_regions(string)
+        self.assertEqual(len(regions), 1)
+        r = regions[0]
+        self.assertEqual(r['start_index'], 11)
+        self.assertEqual(r['end_index'], 13)
+
+    def test_detect_print(self):
+        string = "<html> <body> <code>print(len([1,2,3,4]))</code> </body> </html>"
+        regions = self.get_regions(string)
+
+        self.assertEqual(len(regions), 1)
+        r = regions[0]
+        self.assertEqual(r['start_index'], 6)
+        self.assertEqual(r['end_index'], 8)
--- tutorons/tests/python/test_query.py
+++ tutorons/tests/python/test_query.py
@@ -11,7 +11,7 @@ from django.test import Client
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
-class FetchExplanationsForPythonBuiltInsTest(unittest.TestCase):
+class FetchAllExplanationsTest(unittest.TestCase):
 
     def setUp(self):
         self.client = Client()
@@ -23,7 +23,7 @@ class FetchExplanationsForPythonBuiltInsTest(unittest.TestCase):
         regions = json.loads(resp.content)
         return regions
 
-    def test_single_built_in(self):
+    def test_get_region(self):
         string = "<html> <body> <code>abs(2)</code> </body> </html>"
         regions = self.get_regions(string)
 
@@ -34,8 +34,12 @@ class FetchExplanationsForPythonBuiltInsTest(unittest.TestCase):
             'HTML > BODY:nth-of-type(1) > CODE:nth-of-type(1)')
         self.assertEqual(r['start_index'], 0)
         self.assertEqual(r['end_index'], 2)
+        self.assertIn(
+            "Return the absolute value of a number.",
+            BeautifulSoup(r['document']).text
+        )
 
-    def test_multiple_built_ins(self):
+    def test_get_multiple_regions(self):
         string = "<html> <body> <code>abs(2)\nlen('fdsjkfds')\nbin(1)</code> </body> </html>"
         regions = self.get_regions(string)
 
@@ -46,6 +50,10 @@ class FetchExplanationsForPythonBuiltInsTest(unittest.TestCase):
             'HTML > BODY:nth-of-type(1) > CODE:nth-of-type(1)')
         self.assertEqual(r0['start_index'], 0)
         self.assertEqual(r0['end_index'], 2)
+        self.assertIn(
+            "Return the absolute value of a number.",
+            BeautifulSoup(r0['document']).text
+        )
 
         r1 = regions[1]
         self.assertEqual(
@@ -53,45 +61,27 @@ class FetchExplanationsForPythonBuiltInsTest(unittest.TestCase):
             'HTML > BODY:nth-of-type(1) > CODE:nth-of-type(1)')
         self.assertEqual(r1['start_index'], 7)
         self.assertEqual(r1['end_index'], 9)
+        self.assertIn(
+            "Return the length (the number of items) of an object.",
+            BeautifulSoup(r1['document']).text
+        )
 
-        r2 = regions[2]
-        self.assertEqual(
-            r2['node'],
-            'HTML > BODY:nth-of-type(1) > CODE:nth-of-type(1)')
-        self.assertEqual(r2['start_index'], 23)
-        self.assertEqual(r2['end_index'], 25)
 
-    def test_binary_op_built_ins(self):
-        string = "<html> <body> <code>abs(2) + len('fdsjkfds') </code> </body> </html>"
-        regions = self.get_regions(string)
+class FetchExplanationForPlaintextTest(unittest.TestCase):
 
-        self.assertEqual(len(regions), 2)
-        r0 = regions[0]
-        self.assertEqual(r0['start_index'], 0)
-        self.assertEqual(r0['end_index'], 2)
+    def setUp(self):
+        self.client = Client()
 
-        r1 = regions[1]
-        self.assertEqual(r1['start_index'], 9)
-        self.assertEqual(r1['end_index'], 11)
+    def get_explanation(self, text):
+        resp = self.client.post(
+            '/python/explain',
+            data={'origin': 'www.test.com', 'text': text})
+        return resp.content
 
-    def test_ignore_malformed_calls(self):
-        string = "<html> <body> <code>abs(2</code> </body> </html>"
-        regions = self.get_regions(string)
-        self.assertEqual(len(regions), 0)
+    def test_explain_python_builtin_from_plaintext(self):
+        resp = self.get_explanation('zip')
+        self.assertIn("This function returns a list of tuples,", resp)
 
-    def test_detect_builtin_after_increment(self):
-        string = "<html> <body> <code>x = 2\nx += abs(-1)</code> </body> </html>"
-        regions = self.get_regions(string)
-        self.assertEqual(len(regions), 1)
-        r = regions[0]
-        self.assertEqual(r['start_index'], 11)
-        self.assertEqual(r['end_index'], 13)
-
-    def test_detect_print(self):
-        string = "<html> <body> <code>print(len([1,2,3,4]))</code> </body> </html>"
-        regions = self.get_regions(string)
-
-        self.assertEqual(len(regions), 1)
-        r = regions[0]
-        self.assertEqual(r['start_index'], 6)
-        self.assertEqual(r['end_index'], 8)
+    def test_fail_to_explain_invalid_python_builtin_from_plaintext(self):
+        resp = self.get_explanation('zip()')
+        self.assertIn("'zip()' could not be explained as a python built-in.", resp)
